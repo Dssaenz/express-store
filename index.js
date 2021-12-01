845159a80b8da7dc4b7e66ddb470343a9eabe368
Dssaenz <darwinsaenz105@gmail.com>
2021-12-01 18:34:18 -0500

    initial server express

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('my app');
+const express = require('express');
+
+const app = express();
+const port = 8080;
+
+app.get('/', (req, res) => {
+  res.send('My server is working');
+});
+
+app.listen(port, () => {
+  console.log(`my port ${port}`);
+});
